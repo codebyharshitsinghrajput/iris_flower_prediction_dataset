b20ff4d924b4f2101f71faa83c40687e39a51679
Harshit singh <harshitsinghrajput2001@gmail.com>
2025-12-23 12:53:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle
 
 with open("iris_dataset.pkl",'rb') as f:
-    model=pickle.load(f)
+    model = pickle.load(f)
 
 st.title("Iris Flower Prediction")
 
@@ -12,7 +12,7 @@ sepal_width=st.slider("sepal width(cm)",2.0,4.4)
 petal_length=st.slider("petal lenght(cm)",1.0,6.9)
 petal_width=st.slider("petal width(cm)",0.1,2.5)
 
-if st.button("prediction"):
+if st.button("Predict"):
     input_data=np.array([[sepal_length,sepal_width,petal_length,petal_width]])
     prediction =model.predict(input_data)
     species=['Setosa','Versicolor','Virginica']
